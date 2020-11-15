0556237630d6999d3f386cc5fa9f6e69f78d5c78
KenEvans <ken.evans@bellevuecollege.edu>
2020-11-14 19:41:28 -0800

    added DeleteMCUser route and misc edits

--- multicloudApp/routes/index.js
+++ multicloudApp/routes/index.js
@@ -42,11 +42,15 @@ function User(name,lastname,username,email,password){
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  console.log('Home Page route called');
+
   res.sendFile('index.html');
 });
 
 /* GET all Users. */
 router.get('/AllMCUsers', function(req, res) {
+  console.log('AllMCUsers called');
+
   MCUsers.find({}, (err, result) => {
     if (err) {
       console.log(err);
@@ -60,8 +64,10 @@ router.get('/AllMCUsers', function(req, res) {
 });
 
 /* GET one User by ID */
-router.get('/MCUserByID/:id', function (req, res) {
-  MCUsers.findById({ _id: req.params.id }, (err, result) => {
+router.get('/MCUserByID', function (req, res) {
+  console.log('MCUserByID called');
+
+  MCUsers.findById({ _id: req.body.id }, (err, result) => {
     if (err) {
       console.log(err);
       res.status(500).send(err);
@@ -72,21 +78,24 @@ router.get('/MCUserByID/:id', function (req, res) {
     }
   });
 });
-//MCUserInfo SignIn
+
+/* MCUserInfo SignIn */
 router.post('/MCUserInfo', function (req, res) {
-  console.log('MCUserInfo called')
-  let holdUserName = req.body.username
-  let holdPassword = req.body.password
-  HoldUserData.push(holdUserName,holdPassword)
-  res.json({username: holdUserName, password:holdPassword})
-  return HoldUserData
-})
+  console.log('MCUserInfo called');
+
+  let holdUserName = req.body.username;
+  let holdPassword = req.body.password;
+  HoldUserData.push(holdUserName,holdPassword);
+  res.json({username: holdUserName, password:holdPassword});
+  return HoldUserData;
+});
+
 /* GET one User by UserName and Password */
 router.get('/MCUserByUsrNmPwd', function (req, res) {
-  console.log("username from node " + HoldUserData[0])
-  console.log("password from node " + HoldUserData[1])
-  req.body.username = HoldUserData[0]
-  req.body.password = HoldUserData[1]
+  console.log("username from node " + HoldUserData[0]);
+  console.log("password from node " + HoldUserData[1]);
+  req.body.username = HoldUserData[0];
+  req.body.password = HoldUserData[1];
   
   MCUsers.findOne({
     username: req.body.username,
@@ -128,8 +137,10 @@ router.post('/MCUser', function(req, res) {
 });
 
 /* update one User */
-router.patch('/UpdateMCUser/:id', function (req, res) {
-  MCUsers.findById({ _id: req.params.id }, async (err, result) => {
+router.patch('/UpdateMCUser', function (req, res) {
+  console.log('UpdateMCUser called');
+
+  MCUsers.findById({ _id: req.body.id }, async (err, result) => {
     if (err) {
       console.log(err);
       res.status(500).send(err);
@@ -137,6 +148,9 @@ router.patch('/UpdateMCUser/:id', function (req, res) {
     else {
       if (req.body.password) {
         result.password = req.body.password
+      }
+      if (req.body.email) {
+        result.email = req.body.email
       }
       if (req.body.lastname) {
         result.lastname = req.body.lastname
@@ -150,138 +164,33 @@ router.patch('/UpdateMCUser/:id', function (req, res) {
         res.status(200).json(newResult);
       }
       else {
-        res.status(404);
-        res.send({ errot: "Update save failed!" });
+        res.status(404).json({ error: "Update save failed!" });
       }
     }
   });
 });
 
 /* delete one User */
-/* router.delete('/DeleteMCUser/:id', function (req, res) {
-  MCUsers.deleteOne({ _id: req.params.id }, (err, result) => { 
+router.delete('/DeleteMCUser', function (req, res) {
+  console.log('DeleteMCUser called');
+
+  MCUsers.findById({ _id: req.body.id }, async (err, result) => {
     if (err) {
       console.log(err);
-      res.status(404).send(err);
+      res.status(500).send(err);
     }
     else {
-      console.log("MCUser successfully deleted");
-      console.log(result);
-      res.status(200).json({ message: "MCUser successfully deleted" });
-    }
-  });
-}); */
-
-  // for this version, we will keep data on server in an array
-heroArray = [];
-
-//constructor
-function Hero(pId, pName, pPower) {
-  this.id= pId;
-  this.name = pName;
-  this.power = pPower;
-  }
-
-  // pre-populate with some data
-heroArray.push( new Hero (11, 'Dr Nice', "Fire") );
-heroArray.push( new Hero (12, 'Narco', "Invisible") );
-heroArray.push( new Hero (13, 'Bombasto', "Fire") );
-heroArray.push( new Hero (14, 'Celeritas',"X-Ray Vision") );
-heroArray.push( new Hero (15, 'Magneta', "Invisible") );
-heroArray.push( new Hero (16, 'RubberMan',"X-Ray Vision") );
-heroArray.push( new Hero (17, 'Dynama', "Invisible") );
-heroArray.push( new Hero (18, 'Dr IQ', "X-Ray Vision") );
-heroArray.push( new Hero (19, 'Magma', "Fire") );
-heroArray.push( new Hero (20, 'Tornado', "Invisible") );
-
-let usersArray = [];
-
-usersArray.push(new User('jack', 'Broadly','jkbrdl','52413'));
-usersArray.push(new User('tom', 'Thumb','lilTom','qwerty'));
-
-router.get('/users', (req,res) => {
-  console.log(usersArray)
-  res.status(200).json(usersArray)
-});
-
-router.get('/heroes', function(req, res) {
-  res.status(200).json(heroArray);
-    console.log(heroArray);
-});
-
-router.get('/heroes/:id', function(req, res) {
-  let found = false;
-    for(var i=0; i < heroArray.length; i++)
-    {
-      if( heroArray[i].id == req.params.id)
-      {
-        console.log(heroArray[i]);
-        found = true
-        res.status(200).json(heroArray[i]);
+      const newResult = await MCUsers.deleteOne({ username: result.username, password: result.password });
+      if (newResult.n == 1 && newResult.ok == 1 && newResult.deletedCount == 1) {
+        console.log(newResult);
+        console.log("MCUser successfully deleted");
+        res.status(200).json(newResult);
+      }
+      else {
+        res.status(404).json({ error: "Delete failed!" });
       }
     }
-    if(found === false){
-      res.status(500).send("no such hero");
-      }
-
   });
-  router.put('/heroes/:id', function(req, res) {
-    var changedHero = req.body; 
-   for(var i=0; i < heroArray.length; i++)
-   {
-     if( heroArray[i].id == req.params.id)
-     {
-       heroArray[i].name = changedHero.name;
-       heroArray[i].power = changedHero.power;
-       console.log(heroArray[i]);
-       found = true
-       res.status(200).json(heroArray[i]);
-     }
-   }
-   if(found === false){
-     res.status(500).send(err);
-   }
- });
-// delete is used to delete existing object
-router.delete('/heroes/:id', function(req, res) {
-  for(var i=0; i < heroArray.length; i++)
-  {
-    if( heroArray[i].id == req.params.id)
-    {
-      heroArray.splice(i,1);
-      found = true
-      res.status(200).json('deleted hero');
-    }
-  }
-  if(found === false){
-    res.status(500).send(err);
-  }
 });
-router.post("/heroes", function(req, res) {
-
-   // sort by id (need to create a new, unique id)
-   heroArray.sort(function(a, b) {
-    return (a.id) - (b.id);
-   });
-   var newID = (heroArray[heroArray.length-1].id) +1;
-   var newHero = new Hero(newID, req.body.name, req.body.power);  // need to fix !!!!!
-   heroArray.push(newHero);
-   res.status(200).json(newHero);  // returns the new hero which the observable 
-  // uses to update the client side array so the display looks correct.
-});
-// router.post("/heroes", function(req, res) {
-
-//   // sort by id (need to create a new, unique id)
-//   heroArray.sort(function(a, b) {
-//    return (a.id) - (b.id);
-//   });
-//  var newID = (heroArray[heroArray.length-1].id) +1;
-
-//  var newHero = req.body;
-//  newHero.id = newID;
-//  heroArray.push(newHero);
-//  res.status(200).json(newHero);  // returns the new hero which the observable 
-//  // uses to update the client side array so the display looks correct.
-// });
 
 module.exports = router;
